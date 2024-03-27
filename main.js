7523a4ed907d83fb475dcc7f3a322f3f3b559755
Carlos Campbells Laptop <carlos.g.campbell@gmail.com>
2024-03-27 13:42:15 -0400

    initialization and start of drop down menu

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+// Drop down menu
+
+/**
+ Conditions!
+
+ 1) The javascript portion needs to be reusable. So if i decide to add more items to the html, I can use the javascript code that I already have setup
+
+**/
